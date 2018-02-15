13300de8eb3f8f9d46a1870b37f4e611b73df477
Craig Johnston <cjimti@gmail.com>
2018-02-15 18:07:16 +0000

    borke out types for external use

--- iotwifi/commands.go
+++ iotwifi/commands.go
@@ -6,29 +6,6 @@ import (
 	"github.com/bhoriuchi/go-bunyan/bunyan"
 )
 
-type SetupCfg struct {
-	DnsmasqCfg       DnsmasqCfg       `json:"dnsmasq_cfg"`
-	HostApdCfg       HostApdCfg       `json:"host_apd_cfg"`
-	WpaSupplicantCfg WpaSupplicantCfg `json:"wpa_supplicant_cfg"`
-}
-
-type DnsmasqCfg struct {
-	Address     string `json:"address"`      // --address=/#/192.168.27.1",
-	DhcpRange   string `json:"dhcp_range"`   // "--dhcp-range=192.168.27.100,192.168.27.150,1h",
-	VendorClass string `json:"vendor_class"` // "--dhcp-vendorclass=set:device,IoT",
-}
-
-type HostApdCfg struct {
-	Ssid          string `json:"ssid"`           // ssid=iotwifi2
-	WpaPassphrase string `json:"wpa_passphrase"` // wpa_passphrase=iotwifipass
-	Channel       string `json:"channel"`        //  channel=6
-	Ip            string `json:"ip"`             // 192.168.27.1
-}
-
-type WpaSupplicantCfg struct {
-	CfgFile string `json:"cfg_file"` // /etc/wpa_supplicant/wpa_supplicant.conf
-}
-
 // Command for device network commands
 type Command struct {
 	Log      bunyan.Logger
@@ -45,9 +22,6 @@ func (c *Command) RemoveApInterface() {
 
 // ConfigureApInterface
 func (c *Command) ConfigureApInterface() {
-	c.Log.Info("GOt HERE %s", c.SetupCfg.HostApdCfg.Ip)
-	
-
 	cmd := exec.Command("ifconfig", "uap0", c.SetupCfg.HostApdCfg.Ip)
 	cmd.Start()
 	cmd.Wait()
